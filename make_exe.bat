a3fe0117ebbe2af10456b88fcdd78386f3c5dd85
ksakai <sakaik@gmail.com>
2025-04-06 23:32:13 +0900

    add .exe file

---
+++ make_exe.bat
@@ -0,0 +1,3 @@
+ 
+ rem pyinstaller --onefile --noconsole --add-data "src/icons/*.svg;icons" --console --distpath release --name ggkplayer src/main.py
+ pyinstaller --onefile --noconsole --add-data "src/icons/*.svg;icons" --noconsole --distpath release --name ggkplayer src/main.py
