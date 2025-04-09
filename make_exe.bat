187eee4ef0654df3b0e82cda9f62efb46d67060a
ksakai <sakaik@gmail.com>
2025-04-09 23:36:43 +0900

    add application icon

--- make_exe.bat
+++ make_exe.bat
@@ -1,3 +1,3 @@
  
  rem pyinstaller --onefile --noconsole --add-data "src/icons/*.svg;icons" --console --distpath release --name ggkplayer src/main.py
- pyinstaller --onefile --noconsole --add-data "src/icons/*.svg;icons" --noconsole --distpath release --name ggkplayer src/main.py
+ pyinstaller --onefile --noconsole --add-data "src/icons/*.svg;icons" --icon=src/icons/ggkplayer.ico --noconsole --distpath release --name ggkplayer src/main.py
